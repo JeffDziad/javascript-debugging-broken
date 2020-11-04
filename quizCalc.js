4bc48e73e7869ce6d3f2bb67fa3bff91e393e5a7
Stacy Read <stacy.read.007@gmail.com>
2020-11-03 23:00:21 -0600

    Initial commit

---
+++ quizCalc.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("form").submit(calculate);
+
+    function calculate(event) {
+        event.preventDefault();
+
+        var quiz1 = parseFloat($("#quiz1").val());
+        var quiz2 = parseFloat($("#quiz2").val());
+        var quiz3 = parseFloat($("#quiz3").val());
+
+        var average = (quiz1 + quiz2 + quiz3) / 3;
+
+        var min = Math.min(quiz1, quiz2, quiz3);
+        var max = Math.max(quiz1, quiz3, quiz3);
+
+        var gradeToPass = (73 * 4) - (quiz1 + quiz2 + quiz3);
+
+        $("#average").text(average.toFixed(1) + "%");
+        $("#minMax").text(`${min}/${max}`);
+        $("#quiz4Grade").text(gradeToPass.toFixed(1));
+    }
+});
